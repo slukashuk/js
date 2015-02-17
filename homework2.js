e614ae36601f722e61d2ceb7a3184b5ffd106fa6
unknown <slukashuk@TWFUA0419.twinfield.com>
2015-02-17 13:47:35 +0200

    Homework2

---
+++ homework2.js
@@ -0,0 +1,12 @@
+var bookTitle = 'A Song of Ice and Fire';
+var authorName = 'George Raymond Martin' ;
+var numberOfPages = 1088;
+var isFiction = true;
+var publishingYear = 2014;
+{
+	console.log ( typeof bookTitle);
+	console.log ( typeof authorName);
+	console.log ( typeof numberOfPages);
+	console.log ( typeof isFiction);
+	console.log ( typeof publishingYear);
+}
