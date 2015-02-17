7b43968d62a407fce05908c87ac24abe0865d68f
unknown <slukashuk@TWFUA0419.twinfield.com>
2015-02-17 13:02:17 +0200

    Homework1

---
+++ homework1.js
@@ -0,0 +1,20 @@
+var genre = 'novel';
+
+
+
+if (genre == 'fiction')
+{
+	console.log('I Robot')
+} 
+else if (genre == 'novel')
+{
+	console.log('Pride and Prejudice')
+} 
+
+else if (genre == 'biography')
+{
+	console.log('The Twelve Caesars')
+} 
+else {
+	console.log('Introduction to Quantum Mechanics')
+}
